1e658b818017bb7b8b419da43f9df7eaa4976bbb
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2023-11-16 13:04:27 +0100

    Feat: adding project home page and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
+import Portfolio from "./components/portfolio/portfolio";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className=' scroll-smooth '>
+      <section
+        className='h-screen snap-start'
+        id='home'
+      >
+        <Navbar />
+        <Hero />
+      </section>
+      <section
+        className='h-screen snap-start'
+        id='about'
+      >
+        <Parallax type='about' />
+      </section>
+      <About />
+      <section
+        className='h-screen snap-start'
+        id='portfolio'
+      >
+        <Parallax type='portfolio' />
+      </section>
+
+      <section className='snap-start'>
+        <Portfolio />
+      </section>
+      <section
+        className='h-screen snap-start'
+        id='contact'
+      >
+        <Contact />
+      </section>
+    </div>
+  );
 };
 
 export default App;
